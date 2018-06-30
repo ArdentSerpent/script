753e1abb53d39ea359eaface765b2c4c2af4568f
ardentserpent <jimo.92@hotmail.com>
2018-06-30 00:59:56 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var subir_bajar = prompt("subes o bajas");
+var pisoActual = 1;
+const primerPiso = 2;
+const ultimoPiso = 20;
+
+for (subir_bajar == "subo") {
+	var pisoSeleccionado = prompt("a que piso quieres ir");
+	 for (subir_bajar; pisoActual <= pisoSeleccionado; pisoActual++ ) {
+	 	alert("llegaste");
+	 }
+}
