d6a1f3e1ca21be8b4d9cfe7f7024dd5f79bf9f66
ardentserpent <jimo.92@hotmail.com>
2018-06-30 14:38:39 -0500

    Termine

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-var subir_bajar = prompt("subes o bajas");
-var pisoActual = 1;
-const primerPiso = 2;
+var subir_bajar = prompt("Subes o Bajas?");
+var piso_seleccionado = prompt("a que piso quieres ir");
+const primerPiso = 1;
 const ultimoPiso = 20;
 
-for (subir_bajar == "subo") {
-	var pisoSeleccionado = prompt("a que piso quieres ir");
-	 for (subir_bajar; pisoActual <= pisoSeleccionado; pisoActual++ ) {
-	 	alert("llegaste");
-	 }
+if (subir_bajar === "subo") {
+	var piso_actual = 1;
+	for (piso_actual; piso_actual <= ultimoPiso; piso_actual++) {
+		console.log("Estas en el piso " + piso_actual);
+		if (piso_actual == piso_seleccionado) {
+			alert("Llegaste!");
+		}
+	}
+}else if (subir_bajar === "bajo") {
+	var pisoActual = 20;
+	for (pisoActual; pisoActual >= primerPiso; pisoActual--) {
+		console.log("Estas en el piso " + pisoActual);
+		if (pisoActual == piso_seleccionado) {
+			alert("Llegaste!");
+		}
+	}
 }
